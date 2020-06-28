d571b0ffe9d9e7facb00db100a3c4768db01431a
Ricardo Francisco <ricardo.francisco@focus-bc.com>
2020-06-28 01:56:29 +0100

    .

--- src/DataAccess/MainDbContext.cs
+++ src/DataAccess/MainDbContext.cs
@@ -2,45 +2,24 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 using ProjectRootNamespace.Api.DataAccess.Seed;
-using ProjectRootNamespace.Api.DataAccess.Entities;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Threading;
 using ProjectRootNamespace.Api.Infrastructure.DataAccess;
-using System;
 
 namespace ProjectRootNamespace.Api.DataAccess
 {
-    public class MainDbContext : DbContext
+    public class MainDbContext : BaseDbContext
     {
         // TODO: Register entities DbSets here
-        public DbSet<Product> Products { get; set; }
-
-        private readonly ILoggerFactory _loggerFactory;
-        protected readonly IHostEnvironment _environment;
 
         public MainDbContext(
             DbContextOptions<MainDbContext> options,
-            ILoggerFactory loggerFactory,
-            IHostEnvironment environment) : base(options)
-        {
-            _loggerFactory = loggerFactory;
-            _environment = environment;
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseLoggerFactory(_loggerFactory);
-            base.OnConfiguring(optionsBuilder);
-        }
+            ILoggerFactory logger,
+            IHostEnvironment env) : base(options, logger, env) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // TODO: Call entities OnModelCreating methods here
 
-            modelBuilder.HasPostgresExtension("uuid-ossp");
-            modelBuilder.HasPostgresExtension("postgis");
-            modelBuilder.ApplyGlobalFilters<ISoftDeletableEntity>(x => !x.Deleted);
+            base.OnModelCreating(modelBuilder);
 
             switch (_environment.EnvironmentName)
             {
@@ -56,22 +35,6 @@ namespace ProjectRootNamespace.Api.DataAccess
                 default:
                     throw new System.Exception("Unexpected environment: " + _environment.EnvironmentName);
             }
-        }
-
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-        {
-            // Fail safe mechanism to ensure that all string properties
-            // are trimmed before being saved
-            foreach (var entity in this.ChangeTracker.Entries())
-            {
-                var properties = entity.Properties.ToList().Where(x => x.Metadata.ClrType == typeof(string));
-                foreach (var property in properties)
-                {
-                    property.CurrentValue = property.CurrentValue?.ToString().Trim();
-                }
-            }
-
-            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
---
+++ src/Infrastructure/DataAccess/BaseDbContext.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProjectRootNamespace.Api.Infrastructure.DataAccess
+{
+    public abstract class BaseDbContext : DbContext
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        protected readonly IHostEnvironment _environment;
+
+        public BaseDbContext(
+            DbContextOptions options,
+            ILoggerFactory loggerFactory,
+            IHostEnvironment environment) : base(options)
+        {
+            _loggerFactory = loggerFactory;
+            _environment = environment;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(_loggerFactory);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("uuid-ossp");
+            modelBuilder.HasPostgresExtension("postgis");
+            modelBuilder.ApplyGlobalFilters<ISoftDeletableEntity>(x => !x.Deleted);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            // Fail safe mechanism to ensure that all string properties
+            // are trimmed before being saved
+            foreach (var entity in this.ChangeTracker.Entries())
+            {
+                var properties = entity.Properties.ToList().Where(x => x.Metadata.ClrType == typeof(string));
+                foreach (var property in properties)
+                {
+                    property.CurrentValue = property.CurrentValue?.ToString().Trim();
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
